d52e91f036430e5c91d0b7c4ab18c9ddc747a53c
Swayamz-123 <itzswayam890@gmail.com>
2025-12-28 16:41:59 +0530

    trigger redeploy by modifying the IncidentForm component

--- frontend/src/components/IncidentForm.jsx
+++ frontend/src/components/IncidentForm.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { createIncident, uploadMedia } from "../api/incident.api";
 import MapPreview from "./MapReview";
 import { getDeviceId } from "../utils/deviceId";
-
+//made changes to trigger redeploy
 export default function IncidentForm() {
   const navigate = useNavigate();
 
